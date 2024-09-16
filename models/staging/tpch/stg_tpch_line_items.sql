cf8649b7c5356a0be090d09afee97a1bc1709f1a
Kyle Coapman <kcoapman@dbtlabs.com>
2024-09-16 18:49:57 +0000

    initial build

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -1,6 +1,6 @@
 with source as (
 
-    select * from {{ source('tpch_now', 'lineitem') }}
+    select * from {{ source('tpch', 'lineitem') }}
 
 ),
 
--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -1,7 +1,7 @@
 
 with source as (
 
-    select * from {{ source('tpch_now', 'orders') }}
+    select * from {{ source('tpch', 'orders') }}
 
 ),
 

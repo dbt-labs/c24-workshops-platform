77be1a9393af94ba13e89489213733cd184a9ac0
Shelli White <shelli.white@dbtlabs.com>
2024-10-05 16:51:36 +0000

    Introducing error for build to show previous failures

--- models/demo_examples/materializations/inc_orders.sql
+++ models/demo_examples/materializations/inc_orders.sql
@@ -1,14 +1,1 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='order_id',
-        tags=["frequent_runs"]
-    )
-}}
-
-select * from {{ ref('raw_orders') }}
-
-{% if is_incremental() %}
-    -- this filter will only be applied on an incremental run
-    where modified_at > (select max(modified_at) from {{ this }}) 
-{% endif %}
+ajdflkasjdfsdf
